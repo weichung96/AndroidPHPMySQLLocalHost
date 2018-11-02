cd28a1f696a7afa7a88c64efe2aded3ef7189a9f
unknown <w_chung96@yahoo.com>
2018-11-02 17:28:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/androidphpmysql_localhost/Constants.java
@@ -0,0 +1,10 @@
+package com.example.user.androidphpmysql_localhost;
+
+public class Constants {
+//    private static final String ROOT_URL = "http://192.168.0.150/Android/v1/";
+
+    private static final String ROOT_URL = "http://project300.000webhostapp.com/includes/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+}
